a71c5b2419e5a204b626defea0e034eeebf347a8
Bella9693 <realbella9693@gmail.com>
2025-08-19 20:55:14 -0700

    exercise completed

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// PUT YOUR CODE HERE
+// app.js
+
+// 타임라인 생성
+const tl = gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
+
+// 1단계: 꼭짓점에서 점 → 사각형
+tl.from(".red", {
+  scale: 0,
+  transformOrigin: "top left",
+})
+  .from(".yellow", {
+    scale: 0,
+    transformOrigin: "bottom left",
+  })
+  .from(".green", {
+    scale: 0,
+    transformOrigin: "top right",
+  })
+  .from(".blue", {
+    scale: 0,
+    transformOrigin: "bottom right",
+  });
+
+tl.to(".red", { x: "+=1300", borderRadius: "50%", duration: 2 }, "<")
+  .to(".yellow", { y: "-=450", borderRadius: "50%", duration: 2 }, "<")
+  .to(".green", { y: "+=450", borderRadius: "50%", duration: 2 }, "<")
+  .to(".blue", { x: "-=1300", borderRadius: "50%", duration: 2 }, "<");
